964bee1c92de26c4d2b189245943ca755aba9388
bgm1234 <bgm-asln@hotmail.com>
2024-07-10 11:55:14 +0300

    hosgeldiniz yazısındaki hata düzeltildi.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Archi's Academy Github kursuna hoşgeldniz!
-        </p>
+        <p>Archi's Academy Github kursuna hoşgeldiniz!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
