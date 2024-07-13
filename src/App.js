51c26d4f28f8db1bc801da1640ef23a13668331e
bgm1234 <bgm-asln@hotmail.com>
2024-07-13 22:16:31 +0300

    hosgeldın yazısı ve 2 ozellık eklendi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Archi's Academy Github kursuna hoşgeldiniz! Begum & Alperen </p>
+        <p>HOŞGELDİNİZ</p>
+        <p>özellik eklendi.</p>
+        <p>2. özellik eklendi</p>
         <h2>logo bu alana gelecek</h2>
         <h2>Detaylar bu satıra yazılacak</h2>
         <a
